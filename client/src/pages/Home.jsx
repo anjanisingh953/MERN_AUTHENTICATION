b5c261650c16ec142d7f3d9eb9be2253012a4874
Anjani Singh <anjanisingh953@gmail.com>
2026-03-28 13:54:57 +0530

    Logout component is done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Hero from "../components/Hero";
 import Instructor from "../components/Instructor";
 import Technologies from "../components/Technologies";
@@ -10,7 +10,43 @@ import { Navigate, useNavigate } from "react-router-dom";
 import Footer from "../layout/Footer";
 
 const Home = () => {
-  return <></>;
+
+  const { isAuthenticated, setIsAuthenticated, user, setUser } = useContext(Context);
+
+  const logout = async () => {
+
+    await axios.get("http://localhost:8000/api/v1/user/logout", {
+      withCredentials: true
+    })
+      .then((res) => {
+        toast.success(res.data.message);
+        setUser(null);
+        setIsAuthenticated(false);
+      })
+      .catch((error) => {
+        const message = error.response?.data?.message || error.message || 'Something went wrong';
+        toast.error(message);
+      })
+
+  }
+
+  if(!isAuthenticated){
+    return <Navigate to={'/auth'} />
+  }
+
+  return (
+    <>
+      <section className="home">
+        <Hero />
+        <Instructor />
+        <Technologies />
+        <Footer />
+        <button onClick={logout}>Logout</button>
+      </section>
+    </>
+  )
+
+
 };
 
 export default Home;
